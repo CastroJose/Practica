189fd0db6474a87ee3bab135505f037965ff1475
CastroJose <55367139+CastroJose@users.noreply.github.com>
2019-11-26 17:24:26 -0800

    Add files via upload

---
+++ Pevaluativa1/Persona.cs
@@ -0,0 +1,13 @@
+
+using System;
+namespace Pevaluativa1
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Profesion { get; set; }
+        public int Edad { get; set; }
+              
+    }
+}
